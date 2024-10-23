b2ffc9b55a3f7c10958488aacfc75b342b043517
Dju Makarov <makarovdju@gmail.com>
2024-10-23 23:28:36 +0300

    Added comment

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,8 +7,12 @@ const Home = () => {
 	const decrement = () => setCount((prev) => prev - 1)
 	return (
 		<>
-			<h1>Home</h1>
-			<h2>value = {count}</h2>
+			<h1>Здравствуйте!</h1>
+			<h3 className="secondText">
+				Это первый деплой для моего сайта. Он создан на nginx, чуть позже
+				перейду на Docker и будет больше контента
+			</h3>
+			<h1>value = {count}</h1>
 			<button onClick={increment}>increment</button>
 			<button onClick={decrement}>decrement</button>
 		</>
